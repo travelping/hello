1e6f6ef9fe2474ac57a190e64c7b2aadb8895024
Dmitry Aleksandrov <daleksandrov@ws-dev03.(none)>
2011-02-24 19:35:34 +0100

    client for JSON-RPC/HTTP

--- src/tp_json_rpc.erl
+++ src/tp_json_rpc.erl
@@ -9,7 +9,7 @@
 
 -module(tp_json_rpc).
 
--export([request/2]).
+-export([request/2, call/3]).
 
 request(Service, JSON) when is_list(JSON) ->
     Resp = case tpjrpc_proto:request_json(JSON) of
@@ -22,3 +22,26 @@ request(Service, JSON) when is_list(JSON) ->
                    Error
            end,
     tpjrpc_proto:response_json(Resp).
+
+call(Host, Method, ArgList) when is_list(ArgList) ->
+    Objectencode = tpjrpc_json:encode({obj, [{"method", Method}, {"params", ArgList}, {"id", 1}]}),
+    Objectrequest = {Host, [], "application/json", Objectencode},
+    {ok, {_Line, _Header, Body}} = httpc:request(post, Objectrequest, [], []),
+    {ok, {obj, Props}, _Rest} = tpjrpc_json:decode(Body),
+    case proplists:get_value("error", Props) of
+        null ->
+            Result = proplists:get_value("result", Props),
+            {ok, Result};
+        {obj, ErrorObject} ->
+            case proplists:get_value("code", ErrorObject) of
+                -32600 -> {error, invalid_request};
+                -32601 -> {error, method_not_found};
+                -32602 -> {error, invalid_params};
+                -32603 -> {error, internal_error};
+                Code when (Code >= -32099) and (Code =< -32000) ->
+                    {error, server_error};
+                Code -> {error, Code}
+                 
+            end
+    end.
+
