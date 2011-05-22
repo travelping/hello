1bcad822620058f0a4fcb81289aae157f17c8690
Felix Lange <fjl@twurst.com>
2011-05-22 22:39:14 +0200

    add MIT license header to all source files

--- src/tp_json_rpc.erl
+++ src/tp_json_rpc.erl
@@ -1,22 +1,29 @@
-%    __                        __      _
-%   / /__________ __   _____  / /___  (_)___  ____ _
-%  / __/ ___/ __ `/ | / / _ \/ / __ \/ / __ \/ __ `/
-% / /_/ /  / /_/ /| |/ /  __/ / /_/ / / / / / /_/ /
-% \__/_/   \__,_/ |___/\___/_/ .___/_/_/ /_/\__, /
-%                           /_/            /____/
-%
-% Copyright (c) Travelping GmbH <info@travelping.com>
+% Copyright 2010-2011, Travelping GmbH <info@travelping.com>
 
-%% @doc
-%%    This module contains a JSON-RPC client.
-%% @end
+% Permission is hereby granted, free of charge, to any person obtaining a
+% copy of this software and associated documentation files (the "Software"),
+% to deal in the Software without restriction, including without limitation
+% the rights to use, copy, modify, merge, publish, distribute, sublicense,
+% and/or sell copies of the Software, and to permit persons to whom the
+% Software is furnished to do so, subject to the following conditions:
 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+% DEALINGS IN THE SOFTWARE.
+
+% @doc This module contains a JSON-RPC client.
 -module(tp_json_rpc).
 
 -export([start/0, handle_request/2, call/3, notification/3, call_np/3]).
 
 -include("jrpc_internal.hrl").
-
 -define(TIME_OUT, {timeout, 15000}).
 
 %% @type binary_or_string() = binary() | string()
