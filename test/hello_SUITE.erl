7522fabcffc57549533330e643321f56dec3f0ee
Felix Lange <fjl@twurst.com>
2011-07-05 20:18:41 +0200

    add some basic tests for the new bind API

---
+++ test/hello_SUITE.erl
@@ -0,0 +1,30 @@
+-module(hello_SUITE).
+-compile(export_all).
+
+-include("ct.hrl").
+-include("../include/hello.hrl").
+
+% ---------------------------------------------------------------------
+% -- test cases
+bind_http_url(_Config) ->
+    {ok, _Pid} = hello:bind("http://localhost:5671/test", hello_example_service).
+
+bind_http_url_errors(_Config) ->
+    {ok, _Pid} = hello:bind("http://localhost:5672/test", hello_example_service),
+
+    %% binding the same module returns already_started
+    {error, already_started} = hello:bind("http://localhost:5672/test", hello_example_service),
+
+    %% binding a different one returns occupied
+    {error, occupied} = hello:bind("http://localhost:5672/test", ?MODULE).
+
+% ---------------------------------------------------------------------
+% -- common_test callbacks
+all() -> [bind_http_url, bind_http_url_errors].
+
+init_per_suite(Config) ->
+    hello:start(),
+    Config.
+
+end_per_suite(_Config) ->
+    application:stop(hello).
