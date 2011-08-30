5ee5f9638993584e7760a5b999231819148daa41
Felix Lange <fl@travelping.com>
2011-08-30 22:10:40 +0200

    proper idle timeout behaviour
    session timeout renamed to idle timeout

--- src/hello_stateful_handler.erl
+++ src/hello_stateful_handler.erl
@@ -33,9 +33,10 @@
 % -callback terminate(context(), Reason :: term(), State :: term()) -> term().
 
 -module(hello_stateful_handler).
--behaviour(gen_server).
 -export([start/4, do_binary_request/2, behaviour_info/1,
-         set_session_timeout/1, set_session_timeout/2]).
+         set_idle_timeout/1, set_idle_timeout/2, notify_np/3]).
+
+-behaviour(gen_server).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -include("internal.hrl").
@@ -48,44 +49,52 @@
 behaviour_info(callbacks) -> [{init,2}, {handle_request,4}, {handle_info,3}, {terminate,3}];
 behaviour_info(_Other)    -> undefined.
 
-start(PeerIdentity, HandlerModule, Socket, Args) ->
-    gen_server:start(?MODULE, {PeerIdentity, HandlerModule, Socket, Args}, [{debug, [trace]}]).
+start(TransportMod, TransportState, HandlerMod, HandlerArgs) ->
+    gen_server:start(?MODULE, {TransportMod, TransportState, HandlerMod, HandlerArgs}, []).
 
 do_binary_request(HandlerPid, Request) ->
     gen_server:cast(HandlerPid, {do_binary_request, Request}).
 
-set_session_timeout(Timeout) ->
-    set_session_timeout(self(), Timeout).
+set_idle_timeout(Timeout) ->
+    set_idle_timeout(self(), Timeout).
 
-set_session_timeout(HandlerPid, Timeout) ->
-    gen_server:cast(HandlerPid, {set_session_timeout, Timeout}).
+set_idle_timeout(HandlerPid, Timeout) ->
+    gen_server:cast(HandlerPid, {set_idle_timeout, Timeout}).
+
+notify_np(Context, Method, Args) when is_list(Args) ->
+    notify_np(Context, Method, {Args});
+notify_np(Context, Method, Args = {_}) ->
+    send_notification(Context, Method, Args).
 
 %% --------------------------------------------------------------------------------
 %% -- gen_server callbacks
--record(state, {transport_mod, transport_state, mod, mod_state, session_timeout = infinity}).
+-record(state, {transport_mod, transport_state, mod, mod_state, idle_timer, idle_timeout = infinity}).
 
 init({TransportMod, Transport, HandlerModule, Args}) ->
-    ServerState = #state{mod = HandlerModule,
-                         transport_mod = TransportMod,
-                         transport_state = Transport},
-    case HandlerModule:init(make_context(ServerState), Args) of
+    State0 = #state{mod = HandlerModule, transport_mod = TransportMod, transport_state = Transport},
+    State1 = refresh_idle_timeout(State0),
+    case HandlerModule:init(make_context(State1), Args) of
         {ok, HandlerState} ->
-            {ok, ServerState#state{mod_state = HandlerState}};
+            {ok, State1#state{mod_state = HandlerState}};
         {ok, HandlerState, Timeout} ->
-            {ok, ServerState#state{mod_state = HandlerState}, Timeout};
+            {ok, State1#state{mod_state = HandlerState}, Timeout};
         {error, ErrorReply} ->
             {stop, {error_reply, ErrorReply}}
     end.
 
-handle_cast({set_session_timeout, Timeout}, State) ->
-    {noreply, State#state{session_timeout = Timeout}, Timeout};
+handle_cast({set_idle_timeout, Timeout}, State) ->
+    {noreply, refresh_idle_timeout(State#state{idle_timeout = Timeout})};
+
+handle_cast(idle_timeout, State) ->
+    {stop, idle_timeout, State};
 
 handle_cast({do_binary_request, JSONRequest}, State = #state{mod = Mod, mod_state = ModState}) ->
     case hello_proto:request_json(JSONRequest) of
         {ok, Request} ->
             From = make_from_context(State, Request),
             Result = Mod:handle_request(From, Request#request.method, Request#request.params, ModState),
-            handle_result(Result, true, From, State);
+            NewState = refresh_idle_timeout(State),
+            handle_result(Result, true, From, NewState);
         {error, ErrorResponse} ->
             send_binary(make_context(State), ErrorResponse),
             {stop, badrequest, State}
@@ -108,13 +117,13 @@ code_change(_FromVsn, _ToVsn, State) ->
 %% -- internal functions
 
 handle_result({reply, Reply, NewModState}, true, From, State) ->
-    handle_result({reply, Reply, NewModState, State#state.session_timeout}, true, From, State);
+    handle_result({reply, Reply, NewModState, infinity}, true, From, State);
 handle_result({reply, Reply, NewModState, Timeout}, true, From, State) ->
     ok = send_reply(From, Reply),
     handle_result({noreply, NewModState, Timeout}, true, From, State);
 
 handle_result({noreply, NewModState}, AllowReply, From, State) ->
-    handle_result({noreply, NewModState, State#state.session_timeout}, AllowReply, From, State);
+    handle_result({noreply, NewModState, infinity}, AllowReply, From, State);
 handle_result({noreply, NewModState, Timeout}, _AllowReply, _From, State) ->
     {noreply, State#state{mod_state = NewModState}, Timeout};
 
@@ -132,6 +141,9 @@ send_reply(Context = #context{request = Request}, {ok, Result}) ->
 send_reply(Context = #context{request = Request}, {error, ErrorReply}) ->
     send_binary(Context, hello_proto:response_json(hello_proto:error_response(Request, ErrorReply))).
 
+send_notification(Context, Method, Params) ->
+    send_binary(Context, hello_json:encode({[{'jsonrpc', <<"2.0">>}, {'method', Method}, {'params', Params}]})).
+
 send_binary(#context{transport_mod = TMod, transport_state = TState}, Resp) when is_binary(Resp) ->
     TMod:send(TState, Resp).
 
@@ -139,3 +151,19 @@ make_context(#state{transport_mod = TMod, transport_state = TState}) ->
     #context{transport_mod = TMod, transport_state = TState}.
 make_from_context(#state{transport_mod = TMod, transport_state = TState}, Request) ->
     #context{transport_mod = TMod, transport_state = TState, request = Request}.
+
+refresh_idle_timeout(State) ->
+    case State#state.idle_timer of
+        undefined ->
+            ok;
+        OldTimer ->
+            {ok, cancel} = timer:cancel(OldTimer)
+    end,
+
+    case State#state.idle_timeout of
+        infinity ->
+            State#state{idle_timer = undefined};
+        IdleTimeout ->
+            {ok, NewTimer} = timer:apply_after(IdleTimeout, gen_server, cast, [self(), idle_timeout]),
+            State#state{idle_timer = NewTimer}
+    end.
--- src/hello_stateful_handler_example.erl
+++ src/hello_stateful_handler_example.erl
@@ -1,26 +1,30 @@
 -module(hello_stateful_handler_example).
+-export([bind/0]).
 
 -behaviour(hello_stateful_handler).
 -export([init/2, handle_request/4, handle_info/3, terminate/3]).
 
-init(_Context, [Shell]) ->
-    link(Shell),
-    hello_stateful_handler:set_session_timeout(2000),
-    {ok, Shell}.
+bind() ->
+    hello:bind_stateful("zmq-ipc://hello-events.ipc", ?MODULE, []).
 
-handle_request(From, <<"return_whatever">>, _Args, State) ->
-    {reply, {ok, <<"whatever">>}, State};
-handle_request(_From, _Method, _Args, State) ->
-    {reply, {error, 100, <<"bad method">>}, State}.
+init(_Context, []) ->
+    hello_stateful_handler:set_idle_timeout(5000),
+    {ok, undefined}.
 
-handle_info(Context, timeout, State) ->
-    {stop, oops, State};
-handle_info(Context, {event, Type, EvtParams}, State) ->
-    hello_stateful_handler:notify_np(Context, event, [{'type', Type}, {'params', EvtParams}]),
+handle_request(_From, <<"subscribe">>, _Args, State) ->
+    timer:send_interval(1000, {event, timer}),
+    {reply, {ok, <<"ok">>}, State};
+handle_request(_From, <<"ping">>, _Args, State) ->
     {noreply, State};
+handle_request(_From, _Method, _Args, State) ->
+    {reply, {error, {100, <<"bad method">>}}, State}.
+
+handle_info(Context, {event, timer}, State) ->
+    hello_stateful_handler:notify_np(Context, event, [{'type', timer}]),
+    {noreply, State};
+
 handle_info(_Context, _Info, State) ->
     {noreply, State}.
 
-terminate(_Context, _Reason, State) ->
-    io:format("terminate called~n", []),
+terminate(_Context, _Reason, _State) ->
     ok.
