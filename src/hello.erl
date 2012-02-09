b36f0205f9d81d45ca8a637e08458aaf1e73ec3d
Dmitry Russ <dr@travelping.com>
2012-02-09 17:28:25 +0100

    fix order

--- src/hello.erl
+++ src/hello.erl
@@ -121,7 +121,7 @@ run_stateless_binary_request(CallbackModule, Message, TransportContextParams) ->
     run_stateless_binary_request(hello_proto_jsonrpc, CallbackModule, Message, TransportContextParams).
 
 run_stateless_binary_request(Protocol, CallbackModule, Message, TransportProperties) ->
-    case hello_stateless_handler:run_binary_request(Protocol, CallbackModule, Message, TransportProperties) of
+    case hello_stateless_handler:run_binary_request(Protocol, CallbackModule, TransportProperties, Message) of
         {ok, _Req, Resp} ->
             hello_proto:encode(Resp);
         {error, Resp} ->
