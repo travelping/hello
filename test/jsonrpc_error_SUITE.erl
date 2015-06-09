0bed5de82570138fc4ddb44de9e0163e0a2ed70a
Yury Gargay <yury.gargay@gmail.com>
2015-06-09 17:09:52 +0600

    Fix jsonrpc_error_SUITE

--- test/jsonrpc_error_SUITE.erl
+++ test/jsonrpc_error_SUITE.erl
@@ -4,18 +4,20 @@
 -include_lib("common_test/include/ct.hrl").
 -include("hello_test.hrl").
 -include("../include/jsonrpc_internal.hrl").
+-include("../include/hello.hrl").
 
 % ---------------------------------------------------------------------
 % -- test cases
 binding_not_found(_Config) ->
-	[ {ok, {binding_not_found, _Msg, _Data}} = hello_client:call(ClientName, ?DUMMY_REQ1) || {_, ClientName} <- ?CLIENT_NAMES ].
+	%[ {ok, {binding_not_found, _Msg, _Data}} = hello_client:call(ClientName, ?DUMMY_REQ1) || {_, ClientName} <- ?CLIENT_NAMES ].
+	[ {ok, #error{code = -32601}} = hello_client:call(ClientName, ?DUMMY_REQ1) || {_, ClientName} <- ?CLIENT_NAMES ].
 
 method_not_found(_Config) ->
-	[ {ok, {method_not_found, _Msg, _Data}} = hello_client:call(ClientName, ?DUMMY_REQ2) || {_, ClientName} <- ?CLIENT_NAMES ].
+	[ {ok, #error{code = -32601}} = hello_client:call(ClientName, ?DUMMY_REQ2) || {_, ClientName} <- ?CLIENT_NAMES ].
 
 invalid_params(_Config) ->
-	[ {ok, {invalid_params, _Msg, _Data}} = hello_client:call(ClientName, ?DUMMY_REQ3) || {_, ClientName} <- ?CLIENT_NAMES ],
-	[ {ok, {invalid_params, _Msg, _Data}} = hello_client:call(ClientName, ?DUMMY_REQ4) || {_, ClientName} <- ?CLIENT_NAMES ].
+	[ {ok, #error{code = -32602}} = hello_client:call(ClientName, ?DUMMY_REQ3) || {_, ClientName} <- ?CLIENT_NAMES ],
+	[ {ok, #error{code = -32602}} = hello_client:call(ClientName, ?DUMMY_REQ4) || {_, ClientName} <- ?CLIENT_NAMES ].
 
 error_batch1(_Config) ->
 	[ {ok, [{binding_not_found, _, _}, {binding_not_found, _, _}]} =
@@ -25,21 +27,20 @@ error_batch1(_Config) ->
 
 invalid_request(_Config) ->
     BinRequest = <<"{\"id\":123,\"params\":\"undefined\",\"method\":\"undefined\",\"jsonrpc\":\"2.0\"}">>,
-    %{error, #jsonrpc_response{error = #jsonrpc_error{code = -32600}}} = hello_proto_jsonrpc:decode(BinRequest, [], response).
-    {error, _} = hello_proto_jsonrpc:decode(BinRequest, [], response).
+    {error, #response{response = #error{code = -32600}}} = hello_proto_jsonrpc:decode(BinRequest, [], request).
 
 invalid_notification(_Config) ->
     BinRequest = <<"{\"id\":null,\"params\":\"undefined\",\"method\":\"undefined\",\"jsonrpc\":\"2.0\"}">>,
-    {error, ignore} = hello_proto_jsonrpc:decode(BinRequest).
+    ignore = hello_proto_jsonrpc:decode(BinRequest, [], request).
 
 % ---------------------------------------------------------------------
 % -- common_test callbacks
 all() ->
-    [%binding_not_found,
-     %method_not_found,
-     %invalid_params,
-     %invalid_request,
-     %invalid_notification
+    [binding_not_found,
+     method_not_found,
+     invalid_params,
+     invalid_request,
+     invalid_notification
      ].
 
 init_per_suite(Config) ->
@@ -50,6 +51,7 @@ init_per_suite(Config) ->
     Config.
 
 end_per_suite(_Config) ->
+	unbind_all(),
     application:stop(hello).
 
 % ---------------------------------------------------------------------
@@ -61,16 +63,27 @@ start_named_client(Transport) ->
 	Name = proplists:get_value(Transport, ?CLIENT_NAMES),
 	{Url, TransportOpts} = Transport, 
 	ProtocolOpts = [{protocol, hello_proto_jsonrpc}],
-	{ok, _Pid} = hello_client:start_supervised(Name, Url, TransportOpts, ProtocolOpts, []).
+	{ok, _Pid} = hello_client:start_supervised(Name, Url ++ "/test", TransportOpts, ProtocolOpts, []).
+
+unbind_all() ->
+    [ hello:unbind(Url, CallbackMod) || {Url, _} <- ?TRANSPORTS, CallbackMod <- ?CALLBACK_MODS ],
+    [] = hello_binding:all().
 
 bind_transports() ->
 	[ bind_url(Transport) || Transport <- ?TRANSPORTS ].
 
 bind_url(Transport) ->
-    [ ok = bind_url1(Transport, Handler) || Handler <- ?HANDLER ].
+    [ ok = bind_url1(Transport, Handler) || Handler <- ?CALLBACK_MODS ].
 
 bind_url1({Url, TransportOpts}, Handler) ->
-    [FirstCallback, SecondCallback] = proplists:get_value(Handler, ?CALLBACKS),
     HandlerOpts = proplists:get_value(Handler, ?HANDLER_ARGS),
-    ok = hello:bind(Url, TransportOpts, FirstCallback, Handler, HandlerOpts, hello_proto_jsonrpc, []),
-    ok = hello:bind(Url, TransportOpts, SecondCallback, Handler, HandlerOpts, hello_proto_jsonrpc, []).
+    ProtocolOpts = proplists:get_value(hello_proto_jsonrpc, ?PROTOCOL_ARGS),
+    Self = self(),
+    spawn(fun() ->
+        hello:start_service(Handler, HandlerOpts),
+        hello:start_listener(Url, TransportOpts, hello_proto_jsonrpc, ProtocolOpts, hello_router),
+        true = hello:bind(Url, Handler),
+        Self ! next,
+        receive after infinity -> ok end
+    end), 
+    receive next -> ok end.
