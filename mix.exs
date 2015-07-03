904746abd20381d80b753a81bd32cd9644e7fec5
Yury Gargay <yury.gargay@gmail.com>
2015-07-03 15:30:01 +0600

    Add exometer

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule UniMux.Mixfile do
+defmodule Hello.Mixfile do
   use Mix.Project
 
   def project do
@@ -10,20 +10,22 @@ defmodule UniMux.Mixfile do
   end
 
   def application do
-    [applications: [:lager, :cowboy, :ex_uri, :ezmq, :dnssd, :hackney, :jsx],
+    [applications: [:lager, :exometer_core, :cowboy, :ex_uri, :ezmq, :dnssd, :hackney, :jsx],
      mod: {:hello, []}]
   end
 
   defp deps(_) do
-    [{:lager,   "~> 2.1.1", override: true},
-     {:cowboy,  "~> 1.0.2"},
-     {:yang,    github: "travelping/yang", branch: "master"},
-     {:hackney, "~> 1.1.0"},
-     {:ex_uri,  github: "heroku/ex_uri", branch: "master"},
-     {:ezmq,    github: "RoadRunnr/ezmq", branch: "fix-socket-crash"},
-     {:jsx,     "~> 2.6.2"},
-     {:msgpack, github: "msgpack/msgpack-erlang", branch: "master"},
-     {:dnssd,   github: "benoitc/dnssd_erlang", branch: "master"},
-     {:meck,    "~> 0.8.2"}]
+    [{:lager,         "~> 2.1.1", override: true},
+     {:cowboy,        "~> 1.0.2"},
+     {:yang,          github: "travelping/yang", branch: "master"},
+     {:hackney,       "~> 1.1.0"},
+     {:ex_uri,        github: "heroku/ex_uri", branch: "master"},
+     {:ezmq,          github: "RoadRunnr/ezmq", branch: "fix-socket-crash"},
+     {:jsx,           "~> 2.6.2"},
+     {:msgpack,       github: "msgpack/msgpack-erlang", branch: "master"},
+     {:dnssd,         github: "benoitc/dnssd_erlang", branch: "master"},
+     {:meck,          "~> 0.8.2", override: true},
+     {:exometer_core, github: "Feuerlabs/exometer_core", branch: "master"},
+     {:edown,         github: "uwiger/edown", branch: "master", override: true}]
   end
 end
